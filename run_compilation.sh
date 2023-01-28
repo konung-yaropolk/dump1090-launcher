6f1b8d5d4ea955f0fa82b246a01c0d96d9915314
konung-yaropolk <yaropolk1995@gmail.com>
2023-01-28 04:47:35 -0600

    Update run_compilation.sh

--- run_compilation.sh
+++ run_compilation.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 cd dump1090
 
-# BLADERF=no will disable bladeRF support and remove the dependency on libbladeRF.
-# RTLSDR=no will disable rtl-sdr support and remove the dependency on librtlsdr.
-# HACKRF=no will disable HackRF support and remove the dependency on libhackrf.
-# LIMESDR=no will disable LimeSDR support and remove the dependency on libLimeSuite.
+# RTLSDR=no    will disable rtl-sdr support and remove the dependency on librtlsdr.
+# HACKRF=no    will disable HackRF support and remove the dependency on libhackrf.
+# LIMESDR=no   will disable LimeSDR support and remove the dependency on libLimeSuite.
+# BLADERF=no   will disable bladeRF support and remove the dependency on libbladeRF.
 
 make BLADERF=no HACKRF=no LIMESDR=no
 
