c35bec0f280550954fbcd2a21eea0406846c30c8
konung-yaropolk <yaropolk1995@gmail.com>
2024-04-30 04:35:22 -0500

    modified:   run/run_all.sh

--- run/run_all.sh
+++ run/run_all.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
 cd ..
-./dump1090 --quiet --metric --lat '41.67151' --lon '-91.52516' --write-json public_html/data & cd public_html; python -m http.server 80
+mkdir public_html/data
+./dump1090 --quiet --metric --lat '41.67151' --lon '-91.52516' --write-json public_html/data & cd public_html; python -m http.server 8000
